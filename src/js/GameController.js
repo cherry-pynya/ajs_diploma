a19921dded066c363ab72000a6d1aefce84542ae
cherry-pynya <nikita.cherepnya@gmail.com>
2021-04-15 23:27:22 +0300

    character positioned

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -1,15 +1,67 @@
 import themes from './themes';
+import { generateTeam } from './generators';
+import Bowman from './classes/Bowman';
+import Swordsman from './classes/Swordsman';
+import Magician from './classes/Magician';
+import Daemon from './classes/Daemon';
+import Undead from './classes/Undead';
+import Vampire from './classes/Vampire';
+import PositionedCharacter from './PositionedCharacter';
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
+    this.boardSize = gamePlay.boardSize;
   }
 
   init() {
+    // рисуем поле
     this.gamePlay.drawUi(themes.prairie);
+    // создаем команды
+    const userTeam = generateTeam([Bowman, Magician, Swordsman], 4, 2);
+    const enemyTeam = generateTeam([Daemon, Undead, Vampire], 4, 2);
+    console.log(userTeam, enemyTeam);
+    // расставляем персонажей
+    const positions = [];
+    for (let i = 0; i < userTeam.members.length; i += 1) {
+      const placedChar = new PositionedCharacter(userTeam.members[i], this.getPlayerPosition());
+      positions.push(placedChar);
+    }
+    for (let i = 0; i < enemyTeam.members.length; i += 1) {
+      const placedChar = new PositionedCharacter(enemyTeam.members[i], this.getEnemyPosition());
+      positions.push(placedChar);
+    }
+    console.log(positions);
+    // рисуем расставленных персонажей
+    this.gamePlay.redrawPositions(positions);
+    // листенер для клетки
+    this.gamePlay.
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
+  }
+
+  // генерация позиций игрока
+  getPlayerPosition() {
+    const field = this.boardSize ** 2;
+    const index = Math.floor(Math.random() * field);
+    if (index % this.boardSize === 0 || index % this.boardSize === 1) {
+      return index;
+    }
+    return this.getPlayerPosition();
+  }
+
+  // генерация позиций врага
+  getEnemyPosition() {
+    const field = this.boardSize ** 2;
+    const index = Math.floor(Math.random() * field);
+    if (
+      (index + 1) % this.boardSize === 0
+      || (index + 1) % this.boardSize === this.boardSize - 1
+    ) {
+      return index;
+    }
+    return this.getEnemyPosition();
   }
 
   onCellClick(index) {
--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,4 @@
+import Team from './Team';
 /**
  * Generates random characters
  *
@@ -7,8 +8,17 @@
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
+  const randomTypes = Math.floor(Math.random() * allowedTypes.length);
+  const level = Math.floor(1 + Math.random() * maxLevel);
+  yield new allowedTypes[randomTypes](level);
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  const team = new Team();
+  for (let i = 0; i < characterCount; i += 1) {
+    const character = characterGenerator(allowedTypes, maxLevel).next().value;
+    team.add(character);
+  }
+  return team;
 }
